83addfaa5f93aa3725156c97d31ad7d619d6d419
Marin-WorkPC <marin.pignarre@orange.fr>
2022-05-24 14:22:34 +0200

    first commit

---
+++ module 4/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "auth4f35b951": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
